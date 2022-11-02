9cbe68f9ea4ebf78659bb54b9508fb1c70bdbf59
gabbygood <gabriellegoylan@gmail.com>
2022-11-02 11:37:25 +0800

    first commit

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Ready namo sa midterm exam?",
+    answer: "Yes",
+    options: [
+      "Bagsak nako",
+      "Yes",
+      "No",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Kinsa crush ni jan ruel nacua?",
+    answer: "Short Haired Chinita",
+    options: [
+      "Short Haired Chinita",
+      "Fanny",
+      "Nicka",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Gwapo si Gabby?",
+    answer: "All of the above",
+    options: [
+      "Oo. kaau",
+      "Yes",
+      "Super Yes",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Kinsa crush ni Ken?",
+    answer: "Veronica",
+    options: [
+      "Veronica",
+      "Stacy",
+      "Mimi",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Is Rembrundt so handsome?",
+    answer: "Super!!. Yes",
+    options: [
+      "HAHA",
+      "Super!!. Yes",
+      "No",
+      "None of the above"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    {
+    numb: 6,
+    question: "One final Question!!!...Kinsa crush ni Prince?",
+    answer: "None of the above",
+    options: [
+      "Vi",
+      "Jade",
+      "Jazzy",
+      "None of the above"
+    ]
+  },
+];
